f606885c9f55d1b19ee4941f23b9b946ba3d88ab
Widiya Sihombing <widiya1500016007@webmail.uad.ac.id>
2020-05-23 01:53:17 +0700

    boxes stage

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator</title>
+
+    <link rel="stylesheet" href="assets/dist/css/styles.css">
 </head>
 <body>
-    
+    <div class="wrapper">
+        <div class="title">Calculator</div>
+        <hr>
+        <div class="header">header</div>
+        <hr>
+        <div class="display">display</div>
+        <hr>
+        <div class="control">
+            control
+            <div></div>
+        </div>
+    </div>
 </body>
 </html>
