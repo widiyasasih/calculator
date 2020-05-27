b1f752898778bb43d890f4113e1d60b075953235
Widiya Sihombing <widiya1500016007@webmail.uad.ac.id>
2020-05-27 17:23:08 +0700

    fixing icon box, add blank area to close nav

--- app.php
+++ app.php
@@ -1,10 +1,12 @@
 <div class="about">
     <p class="left">
         # Task 1 <br><br>
-        <h2><b>Calculator Windows Version</b></h2>
+        <h2 style="padding-bottom: 15px;"><b>Calculator Windows Version</b></h2>
     </p>   
     <p>
-        Completed for 4 days at 27 May 2020
+        Estimated complete for 4 days at 27 May 2020
+        <br><br>
+        #EigerTime #BoringTimeChallenge
     </p>
     <br><br><br><br><br><hr>
     <?php require 'src/components/atoms/signature.php' ;?>
@@ -14,7 +16,8 @@
         <p>Calculator Windows</p>
     </div>
     <div style="position:relative">
-        <span href="javascript:void(0)" id="thisCloseHistory" class="closeHistory" onclick="closeHistory()"></span>
+        <div href="javascript:void(0)" id="thisBlankCloseSideBar" class="blankCloseSideBar" onclick="closeNav()">as</div>
+        <div href="javascript:void(0)" id="thisCloseHistory" class="closeHistory" onclick="closeHistory()"></div>
         <?php require 'src/components/molecules/header.php' ;?>
         <?php require 'src/components/molecules/display.php' ;?>
         <?php require 'src/components/molecules/control.php' ;?>
